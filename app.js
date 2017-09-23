1f8d842adcc560218d7300cfac3bae1d99884c31
Ashish kumar <shrivastav.ashish016@gmail.com>
2017-09-23 16:43:47 +0530

    Creating a .txt file to greet current user using fs and os require modules

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+// open nodejs.org/api to view all available api's .
+
+// Requiring first module
+const fs = require('fs');
+const os = require('os');
+var user = os.userInfo();
+
+fs.appendFile('greeting.txt', `Hello ${user.username}!!`);
