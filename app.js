d5ad56f071b55293c8e045e697ed9418860243f5
Ashish kumar <shrivastav.ashish016@gmail.com>
2017-09-25 20:02:03 +0530

    Debugging node application using command line

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-// Using yargs module, we can get user input in a parsed and easy manner so that it is easy to use in the application.
+console.log('Starting app.js');
+
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv  = yargs.argv;
+const argv = yargs.argv;
 var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-if(command === 'add') {
-    var note = notes.addNote(argv.title, argv.body);
-    if(note) {
-        console.log(`Note added with title: ${note.title}`);
-    } else {
-        console.log('Note title already in use.. please try a different one..!!!');
-    }
-} else if(command === 'list') {    
-    console.log('Available notes are: ', notes.getAll());
-} else if(command === 'read') {
-    var note = notes.getNote(argv.title);
-    if(note) {
-        console.log(note);
-    } else {
-        console.log('Note with this title does not exists');
-    }    
-} else if(command === 'remove') {
-    var status = notes.removeNote(argv.title);
-    if(status) {
-        console.log('Note deleted!!');
-    } else {
-        console.log('Note with this title does not exists');
-    }
+if (command === 'add') {
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+  } else {
+    console.log('Note title taken');
+  }
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note found');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
+} else if (command === 'remove') {
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
 } else {
-    console.log('Command not recognized');
+  console.log('Command not recognized');
 }
--- playground/json.js
+++ playground/json.js
@@ -1,14 +1,25 @@
-// While writing json object to file, we first need to convert it to string using JSON.stringify(obj)
-// To read json from file, we first need to parse json using JSON.parse to convert string to JSON Object
+// var obj = {
+//   name: 'Andrew'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Andrew","age": 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
 const fs = require('fs');
-const originalNote = {
-    title : 'My first note',
-    body : 'This is the first note created by Ashish'
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
 };
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
 
-const stringifiedOrignalNote = JSON.stringify(originalNote);
-fs.writeFileSync('notes.json', stringifiedOrignalNote);
-
-const noteString = fs.readFileSync('notes.json');
-const noteObj = JSON.parse(noteString);
-console.log(noteObj.title);
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
